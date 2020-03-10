09f314b0616aa46c852b5c06cb1840f54c1990d6
Ashwini Mani <ashwini.mani@lab49.com>
2020-03-10 14:12:13 +0000

    Use private final fields instaed of @Autowired fields

--- pipeline/src/main/java/com/lab49/bd/integration/IntegrationJobScheduler.java
+++ pipeline/src/main/java/com/lab49/bd/integration/IntegrationJobScheduler.java
@@ -4,17 +4,19 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IntegrationJobScheduler {
-  @Autowired
-  JobLauncher jobLauncher;
 
-  @Autowired
-  Job job;
+  private final JobLauncher jobLauncher;
+  private final Job job;
+
+  public IntegrationJobScheduler(JobLauncher jobLauncher, Job job) {
+    this.jobLauncher = jobLauncher;
+    this.job = job;
+  }
 
   @Scheduled(cron = "${job.schedule.cron.expression}")
   public void perform() throws Exception {
