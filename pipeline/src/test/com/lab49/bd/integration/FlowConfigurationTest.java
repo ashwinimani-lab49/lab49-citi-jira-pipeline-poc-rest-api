931fa61675ace45e5e32d1dc4577469fa7e47f2a
Ashwini Mani <ashwini.mani@lab49.com>
2020-03-11 16:23:47 +0000

    WIP: Add a test to test step

---
+++ pipeline/src/test/com/lab49/bd/integration/FlowConfigurationTest.java
@@ -0,0 +1,54 @@
+package com.lab49.bd.integration;
+
+import java.util.Collection;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = {FlowConfiguration.class, JobLauncherTestUtils.class,
+    JobRepositoryTestUtils.class, StepBuilderFactory.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class})
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+public class FlowConfigurationTest {
+  @Autowired
+  private JobLauncherTestUtils jobLauncherTestUtils;
+
+  @Autowired
+  private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+  @After
+  public void cleanUp() {
+    jobRepositoryTestUtils.removeJobExecutions();
+  }
+
+  @Test
+  public void whenStepTestJsonSchemaExecuted_thenSuccess() throws Exception {
+    JobExecution jobExecution = jobLauncherTestUtils.launchStep("testJsonSchema");
+    Collection stepExecutions = jobExecution.getStepExecutions();
+    ExitStatus exitStatus = jobExecution.getExitStatus();
+
+    assertThat(stepExecutions.size(), is(1));
+    assertThat(exitStatus, is("COMPLETED"));
+  }
+}
